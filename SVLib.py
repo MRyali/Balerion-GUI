9983483a8a0c5d5d14c474ad3396f8b767c337bb
usclpl <berraomar323@gmail.com>
2022-03-31 13:22:05 -0700

    fixed some weird tabs and spaces

--- SVLib.py
+++ SVLib.py
@@ -4,30 +4,29 @@ import time
 
 def set_up_serial(port):
     """
-	SUM: Sets up UART communication with Arduino
-	AUG1: port(string of device port location look in /dev/ for mac and linux. 
-		  It is tty.(random stuff) or Device manager for windows com(#))
-	RETURN: serial object
-	"""
+    SUM: Sets up UART communication with Arduino
+    AUG1: port(string of device port location look in /dev/ for mac and linux. 
+         It is tty.(random stuff) or Device manager for windows com(#))
+    RETURN: serial object
+    """
     ser = serial.Serial(port,baudrate=9600, parity=serial.PARITY_NONE, timeout=1, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
     time.sleep(2)
     return ser
-	
-	
+
 
 def send_data(ser, data_to_send):
-	"""
-	SUM: sends data to the Arduino over UART
-	AUG1: ser (serial object for device to send to) 
-	AUG2: data_to_send (string of data to send)
-	RETURN: None
-	"""
-	send_data_list = list(data_to_send)
-	for data_byte in send_data_list:
-		ser.write(data_byte.encode('ascii'))
+    """
+    SUM: sends data to the Arduino over UART
+    AUG1: ser (serial object for device to send to) 
+    AUG2: data_to_send (string of data to send)
+    RETURN: None
+    """
+    send_data_list = list(data_to_send)
+    for data_byte in send_data_list:
+        ser.write(data_byte.encode('ascii'))
         
 def receive_data(ser,byte=False):
-	
+
     data = ''
 
     if byte == True:
